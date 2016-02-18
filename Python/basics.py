8ed30b15b796e9a3ea445a9b5ff6e92fce376ea1
Santosh Desiraju <desirajusantosh@gmail.com>
2016-02-18 09:36:10 -0800

    Create basics.py

---
+++ Python/basics.py
@@ -0,0 +1,14 @@
+#Dict
+new_dict = {}
+new_dict['key1'] = 'value1'
+new_dict.get('key1')
+new_dict.items()
+new_dict.keys()
+new_dict.values()
+
+#File IO
+f = open(filename, 'mode')
+f.close()
+
+for line in f:
+  print 'line'
