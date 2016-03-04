fd16c18d59c41646e6fabc8a222d111134e9479a
Santosh Desiraju <desirajusantosh@gmail.com>
2016-03-03 16:28:22 -0800

    Create basics.pl

---
+++ Perl/basics.pl
@@ -0,0 +1,48 @@
+#!/bin/perl
+print "this is perl\n";
+print "Perl has 3 type of variables\n Scalar \t Array \t Hash\n";
+
+$a = 10;
+$name = "king";
+$avg = 7.7;
+print "Variables do not have any type";
+
+print "Use double quotes to substitute variables with its value";
+
+$expense = '$10';
+print "My pay is $expense\n"
+
+print "String concatenated using . "
+
+$a = $b x3;
+
+Print "BA"."NA"x2;
+
+print << Verilog;
+module dff ();
+
+Verilog
+
+
+Print "List is colection of ordered elements";
+Print "Array is the variable which holds list";
+
+
+Print"\@ denotes arrays";
+
+$months[0]; #First element
+
+
+@dollar = qw(australia, canada, hong kong);
+
+
+print "pop and push work on the last element of the array";
+print "shift and unshift work on the first element of the array";
+
+
+print "sort keyword used to sort arrays";
+@main = qw(brian, chelsey, lai, bri);
+@main_sort = sort @main;
+
+@dollar = qw(australia, canada, hong kong);
+@middle = splice (@dollar, 2,3);
